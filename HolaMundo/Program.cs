085c0294f55e89851c495d06f594f8ca6d4f1479
Gorostiola <goroztiola@gmail.com>
2024-08-29 12:04:11 -0600

    Carga de codigo

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nombres = {"Jesus", "Goros", "Enriquez"};
+
+            foreach(string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
